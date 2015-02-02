c5fc69069d65e5fe5ed367e0c0679c3f159f79a0
Brian Davidson <briandaviddavidson@yahoo.com>
2015-02-01 21:45:12 -0500

    Update map

--- js/map.js
+++ js/map.js
@@ -1,12 +1,16 @@
+var southWest = L.latLng(39.902990, -78.394562);
+var northEast = L.latLng(37.902990, -76.394562);
+var bounds = L.latLngBounds (southWest, northEast)
+
 var map = L.map('map', {
-    zoomControl: false
+    zoomControl: false,
+    maxBounds: bounds
 }).setView([38.902990, -77.394562], 11);
 
 var tiles = L.tileLayer('http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Tiles © <a href="http://www.stamen.com">stamen design</a>',
 }).addTo(map);
 
-map.dragging.disable();
 map.touchZoom.disable();
 map.doubleClickZoom.disable();
 map.scrollWheelZoom.disable();
