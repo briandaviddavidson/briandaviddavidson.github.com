f70b71ba760525ce3db49cc3453f0a8b3d810f65
Brian Davidson <briandaviddavidson@yahoo.com>
2017-10-01 14:04:39 -0400

    Update map location on contact page

--- js/map.js
+++ js/map.js
@@ -1,23 +1,19 @@
-var southWest = L.latLng(39.902990, -78.394562);
-var northEast = L.latLng(37.902990, -76.394562);
+var southWest = L.latLng(39.391485, -77.585378);
+var northEast = L.latLng(38.391485, -76.585378);
 var bounds = L.latLngBounds (southWest, northEast)
 
 var map = L.map('map', {
     zoomControl: false,
     maxBounds: bounds
-}).setView([38.9586310, -77.3570030], 11);
+}).setView([38.891485, -77.085378], 12);
 
 // Set up the OSM layer
-
-L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
-})
 
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYmRkYXZpZHNvbiIsImEiOiJjaW41MWU5bTcwY2k1dXdtNG54cnhlczFsIn0._R6SrAak5_qF8l31JvSBIA')
 .addTo(map);
 
 // add a marker in the given location
-var marker = L.marker([38.9586310,-77.3570030]).addTo(map);
+var marker = L.marker([38.891485, -77.085378]).addTo(map);
 
 // marker.bindPopup("<div> Please feel free to contact me about maps, UI/UX design and development, or if you just want to chat!<br/><br/><li>Email: <a href='mailto:hello@briandaviddavidson.com'>hello@briandaviddavidson.com</a></li><li>Phone: (404) 641-7629</li></div>").openPopup();
 
